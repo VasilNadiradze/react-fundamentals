8908504161fa230636a37c82e95625b373ec6894
VasilNadiradze <vasil.nadiradze@gmail.com>
2024-01-22 18:09:16 +0400

    - States

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import Message from "./components/Message";
+import { useState } from "react";
+import Expand from "./components/Expand";
 
 function App() {
   return (
     <div>
-      <Message />
-      <Message />
-      <Message />
+      <Expand maxChars={10}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et eos nobis
+        dolorum distinctio accusamus voluptatem sequi labore rem eaque mollitia
+        harum quasi optio eum quae, placeat id molestias! Recusandae, tempora?
+        Quidem, libero delectus velit quia error unde alias asperiores
+        voluptatibus voluptatum fugiat numquam autem, quas nobis at et
+        laboriosam excepturi earum fuga officia quod illum ratione harum
+        veritatis. Repudiandae temporibus mollitia alias architecto sunt
+        delectus magnam, optio accusamus animi id obcaecati facere. Eligendi
+        quasi eius culpa aperiam, nesciunt blanditiis esse amet soluta saepe
+        iste, consequatur, consequuntur recusandae iure expedita voluptate harum
+        unde reiciendis ipsum illo inventore! Magni rem fugit quia.
+      </Expand>
     </div>
   );
 }
---
+++ src/components/Expand.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+interface Props {
+  children: string;
+  maxChars?: number;
+}
+
+const Expand = ({ children, maxChars = 100 }: Props) => {
+  const [isExpanded, setExpanded] = useState(false);
+  const handleClick = () => {
+    //setExpanded(!isExpanded);
+  };
+  if (children.length < maxChars) return <p>{children}</p>;
+  const text = isExpanded ? children : children.substring(0, maxChars);
+  return (
+    <p>
+      {text}...
+      <button onClick={() => setExpanded(!isExpanded)}>
+        {isExpanded ? "less" : "more"}
+      </button>
+    </p>
+  );
+};
+
+export default Expand;
